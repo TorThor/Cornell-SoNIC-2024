c2d41a217d25675691a84190ca6b779f9fc762a1
Tor Qureshi <torqureshi@gmail.com>
2024-09-20 14:10:53 -0500

    File restructure

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import cv2
 import matplotlib.pyplot as plt
-from imgData import imgData
-from imgPro import imgPro
-from whiteBalance import whiteBalance
+from imgProcessing.imgData import imgData
+from imgProcessing.imgPro import imgPro
+from imgProcessing.whiteBalance import whiteBalance
 
 # Setup image variables
 image_name = input("Name of image: ")
-image_path = imgData(image_name, "preProImg").photoFinder()
+image_path = imgData(image_name, "images\preProImg").photoFinder()
 image = cv2.imread(image_path)
 
 # Create processed imgs by calling class methods and save them to proImg
@@ -17,8 +17,8 @@ gaussianBlur_img = imgPro(image).gaussian_blur()
 wb_img = whiteBalance(image).wbCorrection()
 
 # Save gaussianBlur_img & wb_img
-imgData(image_name, "proImg").photoSaver("_wb", wb_img)
-imgData(image_name, "proImg").photoSaver("_gaussianBlur", gaussianBlur_img)
+imgData(image_name, "images\proImg").photoSaver("_wb", wb_img)
+imgData(image_name, "images\proImg").photoSaver("_gaussianBlur", gaussianBlur_img)
 
 # Display imgs for fun ;), though only gaussianBlur_img and wb_img used for threatsLLaVA and depthEstimator
 plt.figure(figsize=(15, 10))
